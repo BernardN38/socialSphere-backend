9960325e661f6c586472aa0302f6ac0a129656b8
bernardn38 <erisboxx@gmail.com>
2022-10-08 22:12:29 -0500

    image service update add uplading to s3; downloading from s3;

--- post-service/helpers/helpers.go
+++ post-service/helpers/helpers.go
@@ -1,7 +1,12 @@
 package helpers
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"log"
 	"net/http"
 	"time"
 )
@@ -34,4 +39,28 @@ func ResponseWithPayload(w http.ResponseWriter, responseCode int, payload []byte
 }
 func ResponseNoPayload(w http.ResponseWriter, responseCode int) {
 	w.WriteHeader(responseCode)
+}
+
+func UploadToS3() {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg.Region = "us-east-1"
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	// Get the first page of results for ListObjectsV2 for a bucket
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("image-service-socialsphere"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("first page results:")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
 }
